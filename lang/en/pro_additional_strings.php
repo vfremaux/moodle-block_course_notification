6c960cecbb7bf4586a423211cb6faf4346b098df
Valery Fremaux <valery.fremaux@gmail.com>
2021-05-06 10:22:37 +0200

    Automated sync from SVN

---
+++ lang/en/pro_additional_strings.php
@@ -0,0 +1,15 @@
+<?php
+
+$string['plugindist'] = 'Plugin distribution';
+$string['plugindist_desc'] = '
+<p>This plugin is the community version and is published for anyone to use as is and check the plugin\'s
+core application. A "pro" version of this plugin exists and is distributed under conditions to feed the life cycle, upgrade, documentation
+and improvement effort.</p>
+<p>Please contact one of our distributors to get "Pro" version support.</p>
+<p><a href="http://www.mylearningfactory.com/index.php/documentation/Distributeurs?lang=en_utf8">MyLF Distributors</a></p>';
+
+require_once($CFG->dirroot.'/blocks/course_notification/lib.php');
+if ('pro' == block_course_notification_supports_feature()) {
+    include($CFG->dirroot.'/blocks/course_notification/pro/lang/en/pro.php');
+    include($CFG->dirroot.'/blocks/course_notification/pro/lang/en/additionals.php');
+}
