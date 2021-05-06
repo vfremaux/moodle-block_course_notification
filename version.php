a77792ad49f6efb557d7bdf5190fe1d9671ea4bc
Valery Fremaux <valery.fremaux@gmail.com>
2021-05-06 10:36:45 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2021030800;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2019051100;        // Requires this Moodle version.
+$plugin->requires  = 2019111200;        // Requires this Moodle version.
 $plugin->component = 'block_course_notification'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.7.0 (Build 2021030800)';
+$plugin->release = '3.8.0 (Build 2021030800)';
 $plugin->maturity = MATURITY_RC;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.7.0005';
+$plugin->codeincrement = '3.8.0005';
 $plugin->privacy = 'dualrelease';
