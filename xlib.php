336245bd6624454be000f12cf7b484beccc086a4
Valery Fremaux <valery.fremaux@gmail.com>
2021-07-28 09:17:24 +0200

    Automated sync from SVN

--- xlib.php
+++ xlib.php
@@ -16,3 +16,23 @@ function block_course_notification_get_site_indicators() {
     include_once($CFG->dirroot.'/blocks/course_notification/pro/lib.php');
     return bcn_get_coldfeedback_site_stats();
 }
+
+/**
+ * Get rendered site indicators from course_notification features.
+ * @param object $course
+ * @return an array of renderered graphs.
+ */
+function block_course_notification_get_course_indicators($course = null) {
+    global $CFG, $COURSE;
+
+    if (is_null($course)) {
+        $course = $COURSE;
+    }
+
+    if (!block_course_notification_supports_feature('notifications/coldfeedback')) {
+        return;
+    }
+
+    include_once($CFG->dirroot.'/blocks/course_notification/pro/lib.php');
+    return bcn_get_coldfeedback_course_stats($course);
+}
